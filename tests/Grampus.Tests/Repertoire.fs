142b0cc0200de9c9aeda0264368168c9cf53c378
Phil Brooks <phil.brooks@asr-services.com>
2021-01-31 13:45:46 +0000

    Update Repertoire.fs

--- tests/Grampus.Tests/Repertoire.fs
+++ tests/Grampus.Tests/Repertoire.fs
@@ -3,14 +3,23 @@ namespace Grampus.Tests
 open FsUnit.MsTest
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open Grampus
+open System.IO
 
 [<TestClass>]
 type Repertoire() =
+    let deffol =
+        Path.Combine
+            (System.Environment.GetFolderPath
+                 (System.Environment.SpecialFolder.MyDocuments), 
+             "Grampus\\repertoire")
     
     [<TestInitialize>]
     member this.Initialize() =
         GrampusInternal.Repertoire.setfol 
             @"D:\GitHub\Grampus\tests\data\repertoire"
+    
+    [<TestCleanup>]
+    member this.Cleanup() = GrampusInternal.Repertoire.setfol deffol
     
     [<TestMethod>]
     member this.White() =
