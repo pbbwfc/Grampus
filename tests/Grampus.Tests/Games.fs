55a56f3c8f5d5f787fdf85ea4d96b0cbf6d0272a
Phil Brooks <phil.brooks@asr-services.com>
2021-02-01 11:12:12 +0000

    Update Games.fs

--- tests/Grampus.Tests/Games.fs
+++ tests/Grampus.Tests/Games.fs
@@ -23,14 +23,14 @@ type Games() =
         let hdrs = Headers.Load tstfol
         let gm = Games.LoadGame tstfol indx.[0] hdrs.[0]
         gm.MoveText.Length |> should equal 107
-        Games.Save tstfol2 [gm;gm]
+        Games.Save tstfol2 [ gm; gm ]
         let indx2 = Index.Load tstfol2
         let hdrs2 = Headers.Load tstfol2
-        indx2.Length|>should equal 2
+        indx2.Length |> should equal 2
         let gm2 = Games.LoadGame tstfol2 indx2.[1] hdrs2.[1]
         gm2.MoveText.Length |> should equal 107
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
-
+    
     [<TestMethod>]
     member this.Add() =
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
@@ -43,14 +43,14 @@ type Games() =
         let hdrs = Headers.Load tstfol
         let gm = Games.LoadGame tstfol indx.[0] hdrs.[0]
         gm.MoveText.Length |> should equal 107
-        Games.Add tstfol2 [gm;gm]
+        Games.Add tstfol2 [ gm; gm ]
         let indx2 = Index.Load tstfol2
         let hdrs2 = Headers.Load tstfol2
-        indx2.Length|>should equal 3
+        indx2.Length |> should equal 3
         let gm2 = Games.LoadGame tstfol2 indx2.[1] hdrs2.[1]
         gm2.MoveText.Length |> should equal 107
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
-
+    
     [<TestMethod>]
     member this.AppendGame() =
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
@@ -66,11 +66,11 @@ type Games() =
         Games.AppendGame tstfol2 gm
         let indx2 = Index.Load tstfol2
         let hdrs2 = Headers.Load tstfol2
-        indx2.Length|>should equal 2
+        indx2.Length |> should equal 2
         let gm2 = Games.LoadGame tstfol2 indx2.[1] hdrs2.[1]
         gm2.MoveText.Length |> should equal 107
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
-
+    
     [<TestMethod>]
     member this.UpdateGame() =
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
@@ -80,19 +80,19 @@ type Games() =
         File.Copy
             (Path.Combine(tstfol, "HEADERS"), Path.Combine(tstfol2, "HEADERS"))
         let indx = Index.Load tstfol
-        indx.[0].Offset |>should equal 0L
+        indx.[0].Offset |> should equal 0L
         let hdrs = Headers.Load tstfol
         let gm = Games.LoadGame tstfol indx.[0] hdrs.[0]
         gm.MoveText.Length |> should equal 107
         Games.UpdateGame tstfol2 0 gm
         let indx2 = Index.Load tstfol2
         let hdrs2 = Headers.Load tstfol2
-        indx2.Length|>should equal 1
-        indx2.[0].Offset |>should equal 1676L
+        indx2.Length |> should equal 1
+        indx2.[0].Offset |> should equal 1676L
         let gm2 = Games.LoadGame tstfol2 indx2.[0] hdrs2.[0]
         gm2.MoveText.Length |> should equal 107
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
-
+    
     [<TestMethod>]
     member this.Compact() =
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
@@ -102,22 +102,22 @@ type Games() =
         File.Copy
             (Path.Combine(tstfol, "HEADERS"), Path.Combine(tstfol2, "HEADERS"))
         let indx = Index.Load tstfol
-        indx.[0].Offset |>should equal 0L
+        indx.[0].Offset |> should equal 0L
         let hdrs = Headers.Load tstfol
         let gm = Games.LoadGame tstfol indx.[0] hdrs.[0]
         gm.MoveText.Length |> should equal 107
         Games.UpdateGame tstfol2 0 gm
         let indx2 = Index.Load tstfol2
         let hdrs2 = Headers.Load tstfol2
-        indx2.Length|>should equal 1
-        indx2.[0].Offset |>should equal 1676L
+        indx2.Length |> should equal 1
+        indx2.[0].Offset |> should equal 1676L
         let gm2 = Games.LoadGame tstfol2 indx2.[0] hdrs2.[0]
         gm2.MoveText.Length |> should equal 107
         Games.Compact tstfol2
         let indx3 = Index.Load tstfol2
         let hdrs3 = Headers.Load tstfol2
-        indx3.Length|>should equal 1
-        indx3.[0].Offset |>should equal 0L
+        indx3.Length |> should equal 1
+        indx3.[0].Offset |> should equal 0L
         let gm3 = Games.LoadGame tstfol2 indx3.[0] hdrs3.[0]
         gm3.MoveText.Length |> should equal 107
         if Directory.Exists(tstfol2) then Directory.Delete(tstfol2, true)
